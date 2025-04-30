24346bee8ca79162723e9a62bb3695839b3ddfb9
Ilona Tykhonovskaya <ilonatiho@gmail.com>
2025-04-30 10:29:37 +0200

    active js

--- script.js
+++ script.js
@@ -50,10 +50,11 @@ function closeMenuMobile() {
 function afficherDifferenceAnnees() {
 
     const anneePublication = 2022;
-    const anneePublication = 2025;
+
     const anneeActuelle = new Date().getFullYear();
     const difference = anneeActuelle - anneePublication;
 
     const divDate = document.getElementById('date-diff');
-    divDate.textContent = `Publié il y a ${difference} an${difference > 1 ? 's' : ''}.`;
+    divDate.textContent = (`Publié il y a ${difference} an${difference > 1 ? 's' : ''}.`);
 }
+afficherDifferenceAnnees()
